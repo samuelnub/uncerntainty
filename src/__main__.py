5b8fdaf5dc047ef59593e41b391e53a33aa66d58
Sam Yap <sry000@student.bham.ac.uk>
2024-09-04 23:39:19 +0100

    Pth check

--- src/__main__.py
+++ src/__main__.py
@@ -1,13 +1,14 @@
 import ROOT
 import Network
 import torch.optim as optim
+import torch
 import os.path
 
 if __name__ == '__main__':
-    dataFilename = 'MasterclassData' #'tmva_class_example'
+    dataFilename = 'MasterclassData.root' #'tmva_class_example.root'
 
 
-    rfile = ROOT.TFile.Open(f'./data/{dataFilename}.root')
+    rfile = ROOT.TFile.Open(f'./data/{dataFilename}')
     for key in rfile.GetListOfKeys():
         name = key.GetName()
         entries = rfile.Get(name).GetEntries()
@@ -29,5 +30,13 @@ if __name__ == '__main__':
 
     modelFilename = f'IV-{'-'.join(ivnames)}-DV-{'-'.join(dvnames)}.pth'
 
+    modelFilepath = f'./model/{modelFilename}'
+
+    if os.path.exists(modelFilepath):
+        # This configuration of IVs and DVs has already been learnt
+        model.load_state_dict(torch.load(modelFilepath))
+    else:
+        # Train fresh model
+        
     
     
